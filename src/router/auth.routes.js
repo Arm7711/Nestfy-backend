1ade6fbf81d85451436cb7fc6bccd487ae707805
Armen Bichakhchyan <armenbichakhchyan.dev@gmail.com>
2026-03-24 23:47:59 +0400

    update auth routes

--- src/router/auth.routes.js
+++ src/router/auth.routes.js
@@ -5,15 +5,26 @@ import {
     refreshToken,
     logout,
     getMe,
+    googleLogin,
+    appleLogin
 } from '../controllers/auth.controller.js';
 import { verifyToken } from '../middleware/auth.middleware.js';
 
 const router = Router();
 
+
 router.post('/register',  register);
 router.post('/login',     login);
+
+
+router.post('/google',    googleLogin);
+router.post('/apple',     appleLogin);
+
+
 router.post('/refresh',   refreshToken);
 router.post('/logout',    logout);
+
+
 router.get('/me',         verifyToken, getMe);
 
 export default router;
